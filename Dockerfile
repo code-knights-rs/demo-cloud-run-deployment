8e2ca2948e1765f89be9cd25774cffc124bf48bd
Raghu <raghu@friday-2.local>
2023-06-14 07:39:31 +0530

    Added demo cloud run code

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use the Python 3.8 slim image as the base image
+FROM python:3.8-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file to the container
+COPY . .
+
+# Install the Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the application code to the container
+COPY . .
+
+# Set the entry point command to run your application
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from flask_mobility import Mobility
+from transformers import MarianMTModel, MarianTokenizer
+
+app = Flask(__name__)
+Mobility(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    output = ""
+    if request.method == 'POST' and  request.MOBILE:
+        text = request.form['text']        # Process the form data
+        result = inference(text=text)
+        output = render_template('mobile_template.html', result=result)
+
+    if request.method == 'POST' and not request.MOBILE:
+        text = request.form['text']  # Process the form data
+        result = inference(text=text)
+        output = render_template('desktop_template.html', result=result)
+
+    return output
+
+
+def inference(text):
+    # Download the model and tokenizer
+    model_name = "models/opus-mt-en-de"  # English to German translation model
+    model = MarianMTModel.from_pretrained(model_name)
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    inputs = tokenizer(text, return_tensors="pt", truncation=True)
+    outputs = model.generate(**inputs)
+    translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return translated_text
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
